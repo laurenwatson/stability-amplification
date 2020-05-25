8187e2eda6d210f47cba1fd5eb0ddacdd808b05b
laurenwatson <watsonla@tcd.ie>
2020-05-25 14:01:54 +0100

    Add logistic regression sensitivity

--- sensitivity.py
+++ sensitivity.py
@@ -3,5 +3,7 @@ import math
 def elastic_net_sensitivity(d, kappa, n, lambda2, model):
     if model in ['svm-hinge']:
         L=1
+    if model in ['log']:
+        L=kappa
     beta=(2*(L**2)*kappa)/(n*lambda2)
     return math.sqrt((2*d*beta)/lambda2)
