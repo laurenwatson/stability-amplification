13e50fc15aa010b2146b5aba254c85b759413d31
laurenwatson <watsonla@tcd.ie>
2020-05-25 14:35:04 +0100

    Add svm-huber sensitivity

--- sensitivity.py
+++ sensitivity.py
@@ -1,7 +1,7 @@
 import math
 
 def elastic_net_sensitivity(d, kappa, n, lambda2, model):
-    if model in ['svm-hinge']:
+    if model in ['svm-hinge', 'svm-huber']:
         L=1
     if model in ['log']:
         L=kappa
